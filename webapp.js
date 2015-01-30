86d29c94c8abff86b695908ea197314679994730
Philippe Wooding <philippe.wooding@gmail.com>
2015-01-30 19:39:33 +0100

    Fix bug with cache parameter that was ignored when received by server. Change hadler to ng-change to that right value is sent.

--- webapp.js
+++ webapp.js
@@ -6,6 +6,7 @@ function sanitizeConfig(config) {
   return {
     url: config.url,
     display_url: config.display_url,
+    cache: config.cache,
     auth: {
       type: config.auth.type,
       privkey: config.auth.privkey,
@@ -20,6 +21,7 @@ module.exports = {
   config: {
     url: String,
     display_url: String,
+    cache: Boolean,
     auth: {
       type: { type: String, enum: ['ssh', 'https', 'http'] },
       privkey: String,
